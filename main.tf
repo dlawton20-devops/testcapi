bf88bdbbda4b37774e2341a3c535a1f7a967da7e
dlawton20-devops <77588468+dlawton20-devops@users.noreply.github.com>
2025-05-02 15:30:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,80 +13,67 @@ provider "rancher2" {
   insecure  = true
 }
 
-resource "rancher2_cluster" "downstream" {
+resource "rancher2_cluster_v2" "downstream" {
   name        = var.cluster_name
   description = "Downstream cluster with proxy mode and Harbor cache"
+  kubernetes_version = "v1.29.1+rke2r1"
 
   rke_config {
-    network {
-      plugin = "canal"
-    }
-
-    # Configure containerd to use Harbor as pull-through cache
-    services {
-      kubelet {
-        extra_args = {
-          "http-proxy"  = var.http_proxy
-          "https-proxy" = var.https_proxy
-          "no-proxy"    = "${var.no_proxy},registry.dci.test.com"
+    machine_pools {
+      name = "control-plane"
+      cloud_credential_secret_name = "openstack-credential"
+      control_plane_role = true
+      etcd_role = true
+      worker_role = false
+      quantity = var.control_plane_count
+      machine_config {
+        kind = "OpenstackConfig"
+        openstack_config {
+          auth_url    = var.openstack_auth_url
+          username    = var.openstack_username
+          password    = var.openstack_password
+          tenant_name = var.openstack_tenant_name
+          region      = var.openstack_region
+          image_name  = var.openstack_image_name
+          flavor_name = var.openstack_flavor_name
+          network_name = var.openstack_network_name
         }
       }
-      containerd {
-        extra_args = {
-          "config-file" = "/etc/rancher/rke2/registries.yaml"
+    }
+
+    machine_pools {
+      name = "worker"
+      cloud_credential_secret_name = "openstack-credential"
+      control_plane_role = false
+      etcd_role = false
+      worker_role = true
+      quantity = var.worker_count
+      machine_config {
+        kind = "OpenstackConfig"
+        openstack_config {
+          auth_url    = var.openstack_auth_url
+          username    = var.openstack_username
+          password    = var.openstack_password
+          tenant_name = var.openstack_tenant_name
+          region      = var.openstack_region
+          image_name  = var.openstack_image_name
+          flavor_name = var.openstack_flavor_name
+          network_name = var.openstack_network_name
         }
       }
     }
 
-    # Add registries.yaml as a file
-    files {
-      name     = "registries.yaml"
-      contents = templatefile("${path.module}/registries.yaml", {
-        HARBOR_USERNAME = var.harbor_username
-        HARBOR_PASSWORD = var.harbor_password
-      })
-      path     = "/etc/rancher/rke2/registries.yaml"
+    # Configure registries
+    registries {
+      configs {
+        hostname = "registry.dci.test.com"
+        auth_config_secret_name = "harbor-auth"
+        insecure_skip_verify = true
+      }
+      mirrors {
+        hostname = "docker.io"
+        endpoints = ["https://registry.dci.test.com"]
+      }
     }
   }
-}
-
-# Node template for OpenStack
-resource "rancher2_node_template" "openstack" {
-  name         = "openstack-template"
-  description  = "OpenStack node template"
-  driver_id    = "openstack"
-  
-  openstack_config {
-    auth_url    = var.openstack_auth_url
-    username    = var.openstack_username
-    password    = var.openstack_password
-    tenant_name = var.openstack_tenant_name
-    region      = var.openstack_region
-    image_name  = var.openstack_image_name
-    flavor_name = var.openstack_flavor_name
-    network_name = var.openstack_network_name
-  }
-}
-
-# Node pool configuration
-resource "rancher2_node_pool" "control_plane" {
-  cluster_id       = rancher2_cluster.downstream.id
-  name             = "control-plane"
-  hostname_prefix  = "control-"
-  node_template_id = rancher2_node_template.openstack.id
-  quantity         = var.control_plane_count
-  control_plane    = true
-  etcd             = true
-  worker           = false
-}
-
-resource "rancher2_node_pool" "worker" {
-  cluster_id       = rancher2_cluster.downstream.id
-  name             = "worker"
-  hostname_prefix  = "worker-"
-  node_template_id = rancher2_node_template.openstack.id
-  quantity         = var.worker_count
-  control_plane    = false
-  etcd             = false
-  worker           = true
 } 
