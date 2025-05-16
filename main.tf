ab1bcfc21340fb17161624c35d00041ead709420
dlawton20-devops <77588468+dlawton20-devops@users.noreply.github.com>
2025-05-16 15:14:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,14 @@ provider "rancher2" {
   insecure  = true
 }
 
-resource "rancher2_catalog_v2" "rook_ceph" {
+resource "rancher2_catalog_v2" "rook_ceph_operator" {
   name     = "rook-ceph"
+  repo_url = "https://charts.rook.io/release"
+  branch   = "main"
+}
+
+resource "rancher2_catalog_v2" "rook_ceph_cluster" {
+  name     = "rook-ceph-cluster"
   repo_url = "https://charts.rook.io/release"
   branch   = "main"
 }
@@ -20,15 +26,27 @@ resource "rancher2_namespace" "rook_ceph" {
 }
 
 locals {
-  rook_ceph_values = templatefile("${path.module}/values.yaml.tftpl", {})
+  rook_ceph_operator_values = templatefile("${path.module}/values-operator.yaml.tftpl", {})
+  rook_ceph_cluster_values  = templatefile("${path.module}/values-cluster.yaml.tftpl", {})
 }
 
-resource "rancher2_app_v2" "rook_ceph" {
-  name          = "rook-ceph"
+resource "rancher2_app_v2" "rook_ceph_operator" {
+  name          = "rook-ceph-operator"
   namespace     = rancher2_namespace.rook_ceph.name
   project_id    = var.project_id
-  repo_name     = rancher2_catalog_v2.rook_ceph.name
+  repo_name     = rancher2_catalog_v2.rook_ceph_operator.name
   chart_name    = "rook-ceph"
   chart_version = "1.12.0"
-  values        = local.rook_ceph_values
+  values        = local.rook_ceph_operator_values
+}
+
+resource "rancher2_app_v2" "rook_ceph_cluster" {
+  name          = "rook-ceph-cluster"
+  namespace     = rancher2_namespace.rook_ceph.name
+  project_id    = var.project_id
+  repo_name     = rancher2_catalog_v2.rook_ceph_cluster.name
+  chart_name    = "rook-ceph-cluster"
+  chart_version = "1.12.0"
+  values        = local.rook_ceph_cluster_values
+  depends_on    = [rancher2_app_v2.rook_ceph_operator]
 } 
